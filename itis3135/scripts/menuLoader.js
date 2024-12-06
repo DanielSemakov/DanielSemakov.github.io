e7ddc6b2569740a276e2457cb68005781cb75f42
Danny Semakov <semakovdaniel@gmail.com>
2024-12-05 22:51:26 -0500

    feat: add project overview and make itis 3135 page look nicer

---
+++ itis3135/scripts/menuLoader.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const includes = document.querySelectorAll('[data-include]');
+
+    includes.forEach(function(div) {
+        const file = div.getAttribute('data-include');
+
+        fetch(file)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('File not found or unable to fetch');
+                }
+                return response.text();
+            })
+            .then(data => {
+                div.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Error loading the file:', error);
+            });
+    });
+});
