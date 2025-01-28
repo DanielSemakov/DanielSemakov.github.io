ecdc8695615663d21536d3ef943a1e941e7314d1
Danny Semakov <semakovdaniel@gmail.com>
2025-01-28 04:54:20 -0500

    feat: improve formatting and interactive canvas

--- front-end/itis3135/scripts/sketch.js
+++ front-end/itis3135/scripts/sketch.js
@@ -1,71 +1,51 @@
-let drawing = false;
-let lines = [];
-let brushColor = "red";
+const redButton = document.getElementById("red-color");
+const greenButton = document.getElementById("green-color");
+const blueButton = document.getElementById("blue-color");
+const purpleButton = document.getElementById("purple-color");
+const whiteButton = document.getElementById("white-color");
+const yellowButton = document.getElementById("yellow-color");
+const pinkButton = document.getElementById("pink-color");
+const clearCanvasButton = document.getElementById("clear-canvas");
+
+const buttons = [redButton, greenButton, blueButton, purpleButton, whiteButton,
+yellowButton, pinkButton];
+
+//When user clicks a color button, change future line color to the color indicated by the
+//button
+const updateLineColor = (event) => {
+  const newColor = event.target.innerHTML.toLowerCase();
+  stroke(newColor);
+};
+
+buttons.forEach((button) => button.addEventListener('click', updateLineColor));
+
+//When user clicks "Clear Canvas" button, clear the canvas
+const clearCanvas = () => {
+  clear();
+  background(0);
+};
+
+clearCanvasButton.addEventListener("click", clearCanvas);
 
 function setup() {
-  createCanvas(windowWidth * 0.95, windowHeight * 0.9);
+  // Create the canvas
+  const canvasWidth = windowWidth * 0.7;
+  const canvasHeight = windowHeight * 0.9;
+
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.parent("canvas-container");
+
+  // Set background to black
   background(0);
-  strokeWeight(5);
-  colorMode(HSB);
+
+  // Set width of the lines
+  strokeWeight(10);
+
+  // Set screen reader accessible description
+  describe('A black canvas where the user draws by selecting a color and dragging the mouse');
 }
 
-function mousePressed() {
-  drawing = true;
-  lines.push([]);
+function mouseDragged() {
+  //Draw a line from the previous position to the current position
+  line(pmouseX, pmouseY, mouseX, mouseY);
 }
-
-function mouseReleased() {
-  drawing = false;
-}
-
-function draw() {
-  for (let i = 0; i < lines.length; i++) {
-    let currentLine = lines[i];
-    stroke(brushColor);
-    noFill();
-    beginShape();
-    for (let j = 0; j < currentLine.length; j++) {
-      vertex(currentLine[j].x, currentLine[j].y);
-    }
-    endShape();
-  }
-
-  if (drawing) {
-    let currentLine = lines[lines.length - 1];
-    currentLine.push(createVector(mouseX, mouseY));
-  }
-}
-
-document.getElementById("clear-canvas").addEventListener("click", () => {
-  background(0);
-  lines = [];
-});
-
-document.getElementById("red-color").addEventListener("click", () => {
-  brushColor = "red";
-});
-
-document.getElementById("green-color").addEventListener("click", () => {
-  brushColor = "green";
-});
-
-document.getElementById("blue-color").addEventListener("click", () => {
-  brushColor = "blue";
-});
-
-document.getElementById("purple-color").addEventListener("click", () => {
-  brushColor = "purple";
-});
-
-document.getElementById("white-color").addEventListener("click", () => {
-  brushColor = "white";
-});
-
-document.getElementById("yellow-color").addEventListener("click", () => {
-  brushColor = "yellow";
-});
-
-document.getElementById("pink-color").addEventListener("click", () => {
-  brushColor = "pink";
-});
-
